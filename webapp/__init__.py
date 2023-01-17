bca8583f20628c7992781cc36c8fe097eab303dc
Igoruxa007 <74563405+Igoruxa007@users.noreply.github.com>
2023-01-17 16:57:27 +0300

    Started to create blueprint 'user'

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, redirect, url_for
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 
 from webapp.forms import LoginForm
 from webapp.model import db, News, User
@@ -35,9 +35,17 @@ def create_app():
     
     @app.route('/login')
     def login():
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
         title = "Авторизация"
         login_form = LoginForm()
         return render_template('login.html', page_title=title, form=login_form)
+    
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        flash('Вы успешно вышли из учетной записи')
+        return redirect(url_for('index'))
     
     @app.route('/process-login', methods=['POST'])
     def process_login():
@@ -45,10 +53,18 @@ def create_app():
         if form.validate_on_submit():
             user = User.query.filter_by(username=form.username.data).first()
             if user and user.check_password(form.password.data):
-                login_user(user)
+                login_user(user, remember=form.remember_me.data)
                 flash('Вы вошли на сайт')
                 return redirect(url_for('index'))
         flash('Неправильное имя пользователя или пароль')
         return redirect(url_for('login'))
+
+    @app.route('/admin')
+    @login_required
+    def admin_index():
+        if current_user.is_admin:
+            return "Hi admin!"
+        else:
+            return "You shall not pass"
 
     return app
