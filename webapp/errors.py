89f166fef2b521c7edabeab3cdc11bf6acfe716f
igoruxa007 <igoruxa007@gmail.com>
2024-05-21 09:19:57 +0300

    Added error handling

---
+++ webapp/errors.py
@@ -0,0 +1,11 @@
+from flask import render_template
+from webapp.model import db
+
+
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
+
+
+def internal_error(error):
+    db.session.rollback()
+    return render_template('errors/500.html'), 500
